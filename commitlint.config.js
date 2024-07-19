262e5faf87f57b967a02e4600851100e2a6e0bc9
atef fawaz <atefawaz@gmail.com>
2024-07-19 13:50:07 +0300

    trying

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,23 +1,22 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-      'type-enum': [
-        2,
-        'always',
-        [
-          'build',
-          'core',
-          'ci',
-          'docs',
-          'feat',
-          'fix',
-          'perf',
-          'refactor',
-          'revert',
-          'style',
-          'test',
-        ],
-      ],
-    },
-  };
-  
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'core',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test'
+      ]
+    ]
+  }
+};
